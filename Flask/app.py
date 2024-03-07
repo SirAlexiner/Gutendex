175213eb833627a154e8dc500b814085e6a8b8e7
Torgrim <sir_alexiner@hotmail.com>
2024-03-07 01:57:13 +0100

    Python, HTML Cleanup

--- Flask/app.py
+++ Flask/app.py
@@ -9,48 +9,55 @@ from bs4 import BeautifulSoup
 # Initialize Flask
 app = Flask(__name__)
 
-def configure_logging(app):
+
+def configure_logging(application):
     # Logging Configuration
-    if app.config['LOG_WITH_GUNICORN']:
+    if application.config['LOG_WITH_GUNICORN']:
         gunicorn_error_logger = logging.getLogger('gunicorn.error')
-        app.logger.handlers.extend(gunicorn_error_logger.handlers)
-        app.logger.setLevel(logging.DEBUG)
+        application.logger.handlers.extend(gunicorn_error_logger.handlers)
+        application.logger.setLevel(logging.DEBUG)
     else:
         file_handler = RotatingFileHandler('instance/flask-user-management.log',
                                            maxBytes=16384,
                                            backupCount=20)
-        file_formatter = logging.Formatter('%(asctime)s %(levelname)s %(threadName)s-%(thread)d: %(message)s [in %(filename)s:%(lineno)d]')
+        file_formatter = logging.Formatter(
+            '%(asctime)s %(levelname)s %(threadName)s-%(thread)d: %(message)s [in %(filename)s:%(lineno)d]')
         file_handler.setFormatter(file_formatter)
         file_handler.setLevel(logging.INFO)
-        app.logger.addHandler(file_handler)
+        application.logger.addHandler(file_handler)
 
     # Remove the default logger configured by Flask
-    app.logger.removeHandler(default_handler)
+    application.logger.removeHandler(default_handler)
 
-    app.logger.info('...Starting the Flask Server...')
+    application.logger.info('...Starting the Flask Server...')
 
 
 # Set a secret key for session management
 app.secret_key = os.urandom(32)
 
-#Get the url to the API from environment variable
+# Get the url to the API from environment variable
 api_url = os.environ.get('API_URL')
 
+
 # Function to check API status
-def API_OK():
+def api_ok():
     try:
         response = requests.get(api_url)
         return response.status_code == 418
     except requests.ConnectionError:
         return False
 
+
+down = 'website_down.html'
+
+
 # Specify Index route of the flask application
 @app.route('/')
 def index():
     # If API is down render website down html
-    if not API_OK():
-        return render_template('website_down.html')
-    
+    if not api_ok():
+        return render_template(down)
+
     # Determine if user is on Mobile of Computer
     # Used to load specific CSS in the HTML templates
     user_agent = request.headers.get('User-Agent')
@@ -63,17 +70,17 @@ def index():
         mobile = ""
 
     # Get the API Query parameters
-    if request.args.get('languages') != None:
+    if request.args.get('languages') is not None:
         language = request.args.get('languages')
     else:
         language = ""
 
-    if request.args.get('search') != None:
+    if request.args.get('search') is not None:
         search_query = request.args.get('search')
     else:
         search_query = ""
 
-    if request.args.get('page') != None:
+    if request.args.get('page') is not None:
         page = request.args.get('page')
     else:
         page = ""
@@ -88,18 +95,20 @@ def index():
 
     # Render HTML template with the fetched data
     return stream_template('index.html',
-                            results=data['results'],
-                            next=data['next'],
-                            previous=data['previous'],
-                            language=language,
-                            search=search_query,
-                            support_languages=support_languages,
-                            mobile=mobile)
+                           results=data['results'],
+                           next=data['next'],
+                           previous=data['previous'],
+                           language=language,
+                           search=search_query,
+                           support_languages=support_languages,
+                           mobile=mobile)
+
+
 @app.route('/bookcount')
 def bookcount():
-    if not API_OK():
-        return render_template('website_down.html')
-    
+    if not api_ok():
+        return render_template(down)
+
     user_agent = request.headers.get('User-Agent')
     if any(keyword in user_agent.lower() for keyword in ['iphone', 'android', 'blackberry', 'mobile', 'linux']):
         if 'ubuntu' in user_agent.lower():
@@ -109,7 +118,7 @@ def bookcount():
     else:
         mobile = ""
 
-    if request.args.get('languages') != None:
+    if request.args.get('languages') is not None:
         language = request.args.get('languages')
     else:
         language = ""
@@ -120,7 +129,7 @@ def bookcount():
     support_languages = response_support_languages.json()
 
     # For bookcount if language parameter is empty default to load:
-    # Chinese, English, French, German, Japanese, Norwegian, Portugese, Russian, Spanish and Finnish
+    # Chinese, English, French, German, Japanese, Norwegian, Portuguese, Russian, Spanish and Finnish
     if language == "":
         language = "zh,en,fr,de,ja,no,pt,ru,es,fi"
 
@@ -132,16 +141,17 @@ def bookcount():
         data = ""
 
     return stream_template('bookcount.html',
-                            results=data,
-                            language=language,
-                            support_languages=support_languages,
-                            mobile=mobile)
+                           results=data,
+                           language=language,
+                           support_languages=support_languages,
+                           mobile=mobile)
+
 
 @app.route('/readership')
 def readership():
-    if not API_OK():
-        return render_template('website_down.html')
-    
+    if not api_ok():
+        return render_template(down)
+
     user_agent = request.headers.get('User-Agent')
     if any(keyword in user_agent.lower() for keyword in ['iphone', 'android', 'blackberry', 'mobile', 'linux']):
         if 'ubuntu' in user_agent.lower():
@@ -165,16 +175,17 @@ def readership():
         data = ""
 
     return stream_template('readership.html',
-                            results=data,
-                            language=language,
-                            support_languages=support_languages,
-                            mobile=mobile)
+                           results=data,
+                           language=language,
+                           support_languages=support_languages,
+                           mobile=mobile)
+
 
 @app.route('/readership/<language>/')
 def readership_language(language):
-    if not API_OK():
-        return render_template('website_down.html')
-    
+    if not api_ok():
+        return render_template(down)
+
     user_agent = request.headers.get('User-Agent')
     if any(keyword in user_agent.lower() for keyword in ['iphone', 'android', 'blackberry', 'mobile', 'linux']):
         if 'ubuntu' in user_agent.lower():
@@ -184,7 +195,7 @@ def readership_language(language):
     else:
         mobile = ""
 
-    if request.args.get('limit') != None:
+    if request.args.get('limit') is not None:
         limit = request.args.get('limit')
     else:
         limit = ""
@@ -201,26 +212,28 @@ def readership_language(language):
         data = ""
 
     return stream_template('readership.html',
-                            results=data,
-                            language=language,
-                            limit=limit,
-                            support_languages=support_languages,
-                            mobile=mobile)
+                           results=data,
+                           language=language,
+                           limit=limit,
+                           support_languages=support_languages,
+                           mobile=mobile)
+
 
 @app.route('/status')
 def status():
-    if not API_OK():
-        return render_template('website_down.html')
-    
+    if not api_ok():
+        return render_template(down)
+
     response = requests.get(f'{api_url}/librarystats/v1/status/')
     status_data = response.json()
     return stream_template('status.html', status=status_data)
 
+
 # special route to fetch and display the books from url on the web service
 @app.route('/fetch_book')
 def fetch_book_page():
-    if not API_OK():
-        return render_template('website_down.html')
+    if not api_ok():
+        return render_template(down)
     url = request.args.get('url')
     if not url:
         return jsonify({'error': 'Missing URL parameter'}), 400
@@ -242,6 +255,7 @@ def fetch_book_page():
         return render_template('book.html', body_content=body_content)
     except requests.RequestException as e:
         return jsonify({'error': str(e)}), 500
+
 
 # Run Flask app
 if __name__ == '__main__':
